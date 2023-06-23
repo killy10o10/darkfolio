9947f60d1e425854e51fe46b6a21a527d4acab6f
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-06-23 03:39:59 +0000

    wip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import profile from '../../public/profile.png'
+import {BsGithub, BsTwitter, BsLinkedin} from 'react-icons/bs' 
 function Header() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center gap-10">
       <div className=" w-40 h-40 flex items-center rounded-full bg-white  border-dark-yellow  border-4">
         <img src={profile} alt="Quami" />
+      </div>
+      <div className="flex flex-col">
+        <h1 className=' font-bold text-3xl tracking-wider'>Quami Killy</h1>
+        <p className=' text-gray-400 tracking-tight'>Software Engineer</p>
+          <BsGithub />
+          <BsTwitter />
+          <BsLinkedin />
       </div>
     </div>
   )
