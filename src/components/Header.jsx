981bc997112c8b70c10275f87e64484151f4780d
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-06-23 22:35:45 +0000

    Hide scrollbar on webkit browsers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import profile from '../../public/profile.png'
 
 function Header() {
   return (
-    <div className="flex items-center gap-10">
+    <div className="flex mt-20 items-center gap-10">
       <div className=" w-40 h-40 flex items-center rounded-full bg-white  border-dark-yellow  border-4">
         <img src={profile} alt="Quami" />
       </div>
-      <div className="flex flex-col">
-        <h1 className=' font-bold text-3xl tracking-wider'>Quami Killy</h1>
+      <div className="flex flex-col gap-2">
+        <h1 className=' font-bold text-3xl tracking-wide'>Quami Killy</h1>
         <p className=' text-gray-400 tracking-tight'>Software Engineer</p>
+      <span className='text-2xl flex  gap-3'>
+          <i className="bi bi-github" />
+          <i className="bi bi-twitter" />
+          <i className="bi bi-instagram" />
+          <i className="bi bi-linkedin" />
+      </span>
       </div>
     </div>
   )
