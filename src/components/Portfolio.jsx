a8bc7505cbfb99abd5828d89a1bb0a25711f391a
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-06-30 01:14:08 +0000

    dynamically apply styling to project cards

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,64 +1,65 @@
-import projectCard from "/projectCard.svg";
-import music610 from "/610music.png";
-import joyfulLearning from "/joyful-learning.png";
-import vanlife from "/vanlife.png";
-import muviWeb from "/muvi-webapp.png";
-import countryGecko from "/react-country-gecko.png";
-import { nanoid } from "nanoid";
+import projectCard from '/projectCard.svg';
+import music610 from '/610music.png';
+import joyfulLearning from '/joyful-learning.png';
+import vanlife from '/vanlife.png';
+import muviWeb from '/muvi-webapp.png';
+import countryGecko from '/react-country-gecko.png';
+import { nanoid } from 'nanoid';
 
 function Portfolio() {
-
   const projects = [
     {
       name: 'sample project',
       image: projectCard,
-      description: 'something about project'
+      description: 'something about project',
     },
     {
       name: 'sample project',
       image: music610,
-      description: 'something about project'
+      description: 'something about project',
     },
     {
       name: 'sample project',
       image: joyfulLearning,
-      description: 'something about project'
+      description: 'something about project',
     },
     {
       name: 'sample project',
       image: vanlife,
-      description: 'something about project'
+      description: 'something about project',
     },
     {
       name: 'sample project',
       image: muviWeb,
-      description: 'something about project'
+      description: 'something about project',
     },
     {
       name: 'sample project',
       image: countryGecko,
-      description: 'something about project'
-    }
+      description: 'something about project',
+    },
   ];
 
-  
   const projectsElement = projects.map((project) => {
-  const cardStyle = {
-    height: '226px',
-    background: `url(${project.image})center center / cover`,
-    borderRadius: "15px",
-  }
-  return(
-          <div style={cardStyle} className="md:w-[362px] cursor-pointer w-[330px] shadow" key={nanoid()}>
-          </div>
-  )
-})
+    const dynamicCardStyle = {
+      height: '226px',
+      borderRadius: '15px',
+      background: `url(${project.image}) center /cover`,
+    };
+    return (
+      <div
+        style={dynamicCardStyle}
+        className="md:w-[362px] w-[330px] grayscale hover:grayscale-0 transition duration-700 cursor-pointer shadow"
+        key={nanoid()}
+      ></div>
+    );
+  });
 
   return (
-   <section className="grid md:grid-cols-2 gap-10 p-5 items-center justify-center  before:absolute before:h-2/5 before:w-1/3 before:rounded-tr-full before:rounded-bl-full before:blur-3xl before:bg-accent-2 before:animate-spin-slower before:-z-10 after:absolute after:h-2/5 after:w-2/5 after:rounded-tr-full after:rounded-bl-full after:blur-3xl after:bg-accent-1/80 after:animate-spin-slow after:-z-10">
+    <section className="grid md:grid-cols-2 gap-10 p-5 mb-10 items-center justify-center  before:absolute before:h-2/5 before:w-1/3 before:rounded-tr-full before:rounded-bl-full before:blur-3xl before:bg-accent-2 before:animate-spin-slower before:-z-10 after:absolute after:h-2/5 after:w-2/5 after:rounded-tr-full after:rounded-bl-full after:blur-3xl after:bg-accent-1/80 after:animate-spin-slow after:-z-10">
       {projectsElement}
-   </section>
-  )
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
