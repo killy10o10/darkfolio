8a384ca0609a15106e12e106563817b91fc0b3c7
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-07-04 19:06:45 +0000

    Change navbar brand

--- src/components/NavigationElement.jsx
+++ src/components/NavigationElement.jsx
@@ -4,7 +4,7 @@ function NavigationElement() {
   return (
     <>
       <Navbar  className="sticky p-4 top-0 z-10 dark:bg-opacity-30 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-b-gray-200">
-        <Navbar.Brand className="font-medium tracking-wider text-lg" href="/">Quami Killy</Navbar.Brand>
+        <Navbar.Brand className="font-bold tracking-tight text-lg" href="/">OK</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <div className="md:flex gap-4 items-center ">
