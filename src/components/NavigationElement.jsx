02ea8ac764f933c07f20d5395f711054f9d3adc6
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-07-07 09:40:22 +0000

    Fix dark mode reset after browser refresh

--- src/components/NavigationElement.jsx
+++ src/components/NavigationElement.jsx
@@ -1,6 +1,10 @@
-import { Navbar, DarkThemeToggle } from 'flowbite-react';
+import { Navbar} from 'flowbite-react';
+import useColorMode from "../hooks/useColorMode";
+import { BsSunFill } from 'react-icons/bs';
+import { HiMoon } from 'react-icons/hi2';
 
 function NavigationElement() {
+  const [colorMode, setColorMode] = useColorMode();
   return (
     <>
       <Navbar  className="sticky p-4 top-0 z-10 dark:bg-opacity-30 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-b-gray-200">
@@ -8,7 +12,12 @@ function NavigationElement() {
         <Navbar.Toggle />
         <Navbar.Collapse>
           <div className="md:flex gap-4 items-center ">
-            <DarkThemeToggle/>
+          <button
+          className="text-lg"
+          onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}
+          >
+             {colorMode === "light" ? <HiMoon/> : <BsSunFill />}
+          </button>
             <Navbar.Link href="#">About</Navbar.Link>
             <Navbar.Link href="/#projects">Projects</Navbar.Link>
             <Navbar.Link href="mailto:killynelly@gmail.com" target="_blank" rel="noreferrer">Contact</Navbar.Link>
